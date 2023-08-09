4f58a53b6911c946df09f62051d12542ce8aa5bf
anonymouswalker <hoanganhtran1998@gmail.com>
2023-08-09 14:46:45 -0400

    enter key to execute detection

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import cv2
 import imutils
 import numpy as np
+import mss
+import pywinauto
+import keyboard
 from PIL import Image
 from tensorflow.keras.models import load_model
 
 output_class = ['down', 'left', 'right', 'up']
-
-# Load the trained model
 model = load_model('trained-model/arrow_orientation_model.h5')
 
-def predict_direction2(input_cv2_image):
+# Load the trained model
 
+def predict_direction2(input_cv2_image):
     # Preprocess the input image from cv2.imread
     target_size = (28, 28)  # Make sure it matches the size your model expects
 
@@ -34,9 +36,9 @@ def predict_direction2(input_cv2_image):
 
 
 
-def detect_directions_from_img(image_path):
+def detect_directions_from_img(image):
     # Read the image from the file
-    image = cv2.imread(image_path)
+    # image = cv2.imread(image_path)
 
     # Convert the image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -73,5 +75,64 @@ def detect_directions_from_img(image_path):
 
 
 # Call the function with the input image path - this image is the cropped KEYS AREA
-dirs = detect_directions_from_img('resources/arrows2.png')
-print(dirs)
+# dirs = detect_directions_from_img('resources/arrows2.png')
+# print(dirs)
+
+
+# area: (left, top, width, height)
+def capture_screenshot_with_app(pid, area):
+    app = pywinauto.Application().connect(process=pid)
+    # window = app.window()  # Get the main window of the application
+    window = app["Audition"]
+    window.set_focus()
+    window_rect = window.rectangle()
+    screenshot_area = {
+        "left": window_rect.left + area[0],
+        "top": window_rect.top + area[1],
+        "width": area[2],
+        "height": area[3],
+    }
+    with mss.mss() as sct:
+        screenshot = sct.grab(screenshot_area)
+        screenshot_np = np.array(screenshot)
+        screenshot_bgr = cv2.cvtColor(screenshot_np, cv2.COLOR_RGBA2RGB)
+        
+        return screenshot_bgr
+
+
+def trigger_screenshot():    
+    process_id = 21672
+    area = (280, 540, 470, 40) # (left, top, width, height)
+
+    # Capture a screenshot of the application window and save it
+    captured_image = capture_screenshot_with_app(process_id, area)
+
+    # Call the function to detect directions from the captured image
+    dirs = detect_directions_from_img(captured_image)
+    print(dirs)
+
+    cv2.imwrite('out/captured.png', captured_image)
+
+
+# Register the trigger function to the desired key press event
+def key_listener(e):
+    if e.event_type == keyboard.KEY_DOWN and e.name == 'enter':
+        trigger_screenshot()
+        
+
+keyboard.hook(callback=key_listener)
+# Keep the script running
+keyboard.wait('esc')  # Wait for the 'esc' key to exit
+
+# Unregister the trigger function
+keyboard.unhook_all()
+
+
+# process_id = 10800
+# area = (280, 540, 470, 40) # (left, top, width, height)
+# captured_img = capture_screenshot_with_app(process_id, area)
+
+
+# image = cv2.imread('out/captured.png')
+# d = detect_directions_from_img(image)
+# print(d)
